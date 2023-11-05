778d43072ff1e2c666efef08a5241dca2c929c85
DevGitGabo <u21209578@utp.edu.pe>
2023-11-04 20:42:39 -0500

    first commit

---
+++ src/main/java/pe/LaCasona/backend_casona/BackendCasonaApplication.java
@@ -0,0 +1,13 @@
+package pe.LaCasona.backend_casona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCasonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCasonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/LaCasona/backend_casona/BackendCasonaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.LaCasona.backend_casona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCasonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
